b26167e36989d400273e9724a24f70090b5d5d66
erkuai <3025868932@qq.com>
2026-01-12 22:03:53 +0800

    add gitignore

--- backend/migrations/20260111152247_initial_schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS media (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    description TEXT,
-    media_type TEXT NOT NULL,
-    url TEXT NOT NULL,
-    thumbnail_url TEXT,
-    created_at DATETIME NOT NULL
-);
